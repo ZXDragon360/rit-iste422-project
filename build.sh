f2927f7e17044931477d618b57461e31872ef3a9
Aaron Kelly <axk3897.rit.edu>
2020-02-08 22:32:50 -0500

    Updated build.sh to move all generated class files to build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar build/src/java/*.java
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar build/test/java/*.java
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-cd build/src/java/
-java RunEdgeConvert
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar -d ./build src/java/*.java
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build -d ./build test/java/*.java
+java -cp build/.:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp ./build/ RunEdgeConvert
