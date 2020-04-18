07c104815faa1254b8e8779a0aa59c947d49eea1
Aaron <axk3897@rit.edu>
2020-04-18 19:26:29 -0400

    updated build file

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar *.java
-java -cp .;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar -d ./build src/main/java/*.java
+javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar/build -d ./build src/test/java/*.java
+java -cp build/.;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp ./build/ RunEdgeConvert
